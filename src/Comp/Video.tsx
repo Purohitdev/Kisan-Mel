63d640dd4edfe1a01ef26f339faf7dfb41de8212
Purohitdev <devp61285@gmail.com>
2025-04-05 19:37:33 +0530

    yt video added

--- src/Comp/Video.tsx
+++ src/Comp/Video.tsx
@@ -1,3 +1,41 @@
+// import React, { useState } from 'react';
+// import { FaPlay } from 'react-icons/fa';
+
+// const Video: React.FC = () => {
+//   const [isPlaying, setIsPlaying] = useState(false);
+
+//   return (
+//     <div
+//       className="h-[60vh] w-full bg-black text-white px-16 py-6 flex justify-center items-center"
+//       // style={{
+//       //   backgroundImage: `url(${agri})`,
+//       //   backgroundSize: 'cover',
+//       // }}
+//     >
+//       <div className="h-full w-full flex justify-center items-center">
+//         {!isPlaying ? (
+//           <button
+//             type="button"
+//             className="border h-20 w-20 rounded-full flex justify-center items-center text-4xl transition-transform transform hover:scale-110 focus:outline-none focus:ring-4 focus:ring-gray-500"
+//             aria-label="Play Video"
+//             onClick={() => setIsPlaying(true)}
+//           >
+//             <FaPlay />
+//           </button>
+//         ) : (
+//           <video  autoPlay className="h-[60vh] w-screen object-cover">
+//             <source src="/video.mp4" type="video/mp4" />
+//             Your browser does not support the video tag.
+//           </video>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Video;
+
+
 import React, { useState } from 'react';
 import { FaPlay } from 'react-icons/fa';
 
@@ -7,10 +45,6 @@ const Video: React.FC = () => {
   return (
     <div
       className="h-[60vh] w-full bg-black text-white px-16 py-6 flex justify-center items-center"
-      // style={{
-      //   backgroundImage: `url(${agri})`,
-      //   backgroundSize: 'cover',
-      // }}
     >
       <div className="h-full w-full flex justify-center items-center">
         {!isPlaying ? (
@@ -23,10 +57,14 @@ const Video: React.FC = () => {
             <FaPlay />
           </button>
         ) : (
-          <video  autoPlay className="h-[60vh] w-screen object-cover">
-            <source src="/video.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+          <iframe
+            className="h-[60vh] w-full"
+            src="https://www.youtube.com/embed/uruY2Y8fejE?autoplay=1&rel=0&modestbranding=1"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
         )}
       </div>
     </div>
@@ -34,4 +72,3 @@ const Video: React.FC = () => {
 };
 
 export default Video;
-
